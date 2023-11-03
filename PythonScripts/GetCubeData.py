ea478c8ca4f63b0c1aedcd15dc24542e89b4f96b
cirion02 <34488326+cirion02@users.noreply.github.com>
2023-11-03 20:18:07 +0100

    Add latest update

--- PythonScripts/GetCubeData.py
+++ PythonScripts/GetCubeData.py
@@ -1,5 +1,5 @@
 import csv
-
+from os import listdir
 
 def CubenameToRelativePath(cubename:str) -> str:
       return f"../CubeArtisanDownloads/{cubename}/{cubename}"
@@ -10,19 +10,27 @@ def CubenameToCsvPath(cubename:str) -> str:
 def CubenameToNamesPath(cubename:str) -> str:
       return CubenameToRelativePath(cubename) + ".txt"
 
-def GetAllCustomImageNames(cubename:str) -> [str]:
-    result = []
-    with open(CubenameToCsvPath(cubename), encoding="utf8") as file:
-        reader = csv.reader(file, delimiter=',', quotechar='"')
-        with open(CubenameToNamesPath(cubename)) as names:
-            for card, name in zip(reader, [""] + names.readlines()):
-                if card[0] == "Name":
-                    continue
-                if "Paper Only" in card[13].split(";"):
-                    continue
+def GetAllCustomImageNames(cubename:str, useCsv=False) -> [str]:
+    if useCsv:
+        result = []
+        with open(CubenameToCsvPath(cubename), encoding="utf8") as file:
+            reader = csv.reader(file, delimiter=',', quotechar='"')
+            with open(CubenameToNamesPath(cubename)) as names:
+                for card, name in zip(reader, [""] + names.readlines()):
+                    if card[0] == "Name":
+                        continue
+                    if "Paper Only" in card[13].split(";"):
+                        continue
 
-                name = name.strip()
+                    name = name.strip()
 
-                if card[11] != "": #Custom Image
-                    result.append(name)
-    return result
+                    if card[11] != "": #Custom Image
+                        result.append(name)
+        return result
+    
+    files = [f[:-4] for f in listdir(f"../ImageFiles/{cubename}")]
+
+    return files
+
+if __name__ == "__main__":
+     print(GetAllCustomImageNames("TrolleyCube"))
